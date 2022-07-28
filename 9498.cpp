5014d51bd268f66ea3a1e6009155f7293ec2cba7
KimDongBeen <85830445+KimDongBeen@users.noreply.github.com>
2022-07-28 11:14:41 +0900

    Create 9498.cpp

---
+++ 9498.cpp
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/9498
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int score;
+    cin >> score;
+    if(score > 89) cout << "A";
+    else if(score > 79) cout << "B";
+    else if(score > 69) cout << "C";
+    else if(score > 59) cout << "D";
+    else cout << "F";
+    return 0;
+}
