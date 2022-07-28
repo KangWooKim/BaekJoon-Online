d4ce9b2b6573c8c0ffa092c7132a5928c1fc9467
KimDongBeen <85830445+KimDongBeen@users.noreply.github.com>
2022-07-28 11:08:55 +0900

    Create 1330.cpp

---
+++ 1330.cpp
@@ -0,0 +1,12 @@
+//https://www.acmicpc.net/problem/1330
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int A, B;
+    cin >> A >> B;
+    if(A > B) cout << ">";
+    else if(A < B) cout << "<";
+    else cout << "==";
+    return 0;
+}
